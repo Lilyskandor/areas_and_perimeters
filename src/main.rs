da2acefd73b1ea7fdfdad4a8821daec88d54026e
Alexandre CAILLIEZ <cailliez.alexandre@hotmail.fr>
2022-11-23 17:53:25 +0200

    Added rectangle area calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+// Alexandre CAILLIEZ
+// Version 1.0.0
+use std::io;
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+
+//    let width = user_input_to_int("Rectangle's width: ");
+//    let height = user_input_to_int("Rectangle's height: ");
+
+    let rect1 = Rectangle {
+        width: user_input_to_int("Rectangle's width: "),
+        height: user_input_to_int("Rectangle's height: "),
+    };
+    println!(
+        "The rectangle has an area of {} square units.",
+        rectangle_area(rect1)
+    );
+}
+
+fn rectangle_area(rectangle: Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+fn user_input_to_int(text_to_display: &str) -> u32 {
+    println!("{}", text_to_display);
+    let mut user_input = String::new();
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read input.");
+
+    let parsed_input: u32 = user_input.trim().parse().expect("Failed to convert to int");
+    parsed_input
 }
